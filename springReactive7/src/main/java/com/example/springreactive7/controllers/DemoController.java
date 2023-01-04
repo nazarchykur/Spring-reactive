31f42d8c50672834aeaeabca47f1464a87107732
NazarChykur <nazar.chykur@emburse.com>
2023-01-04 12:51:44 +0200

    Spring Reactive - Lesson 7 - Securing Reactive apps: get authentication in needed place

--- springReactive7/src/main/java/com/example/springreactive7/controllers/DemoController.java
+++ springReactive7/src/main/java/com/example/springreactive7/controllers/DemoController.java
@@ -1,6 +1,8 @@
 package com.example.springreactive7.controllers;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -10,6 +12,14 @@ public class DemoController {
     
     @GetMapping(value = "/demo", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Mono<String> demo(){
+        /*
+        щоб взяти аутентифікацію, де нам потрібно можна витягнути з  ReactiveSecurityContextHolder
+         */
+        
+//        ReactiveSecurityContextHolder.getContext()
+//                .map(SecurityContext::getAuthentication)
+//                .subscribe(System.out::println);
+        
         return Mono.just("demo");
     }
 
